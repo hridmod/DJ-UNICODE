e394014849bfeb5f7991e60b89e922d56f82809b
Ridmod <hridaymodi@hotmail.com>
2020-08-11 23:19:56 +0530

    fianl 11/08

--- BACKEND/task3/task3.js
+++ BACKEND/task3/task3.js
@@ -14,21 +14,32 @@ app.use(express.static("public"));
 //TRIAL
 var arr1 = [];
 //ROUTES
+app.get("/", function (req, res) {
+  res.redirect("/forms");
+});
 app.get("/forms", (req, res) => {
-  res.render("forms");
+  let student;
+  res.render("forms", { arr1: arr1, student: student });
 });
 
 app.post("/forms", (req, res) => {
-  const data = req.body;
-  arr1.push(data);
-  res.redirect("/preview");
+  arr1.push({
+    firstName: req.body.student.first_name,
+    lastName: req.body.student.last_name,
+    score: { Maths: req.body.student.Maths, English: req.body.student.English },
+  });
+  arr1.sort((a, b) =>
+    (b.score.Maths + b.score.English) / 2 >
+    (a.score.Maths + a.score.English) / 2
+      ? 1
+      : -1
+  );
+  console.log(arr1);
+  res.render("preview", { arr1: arr1 });
 });
 
 app.get("/preview", (req, res) => {
-  arr1.sort((a, b) =>
-    (b.Math + b.English) / 2 > (a.Math + a.English) / 2 ? 1 : -1
-  );
-  res.send(arr1);
+  res.render("preview", { arr1: arr1 });
 });
 
 /////////////////////
